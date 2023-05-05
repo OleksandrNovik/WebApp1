d628825dd90251d25ba7649c5bf676a06a9613da
Novik Oleksandr <novikoleksandr9@gmail.com>
2023-05-06 00:19:34 +0300

    Added BaseController for setting up culture, this can be useful later for changing language of a website

---
+++ WebProject/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Globalization;
+
+namespace WebProject.Controllers
+{
+    /// <summary>
+    /// Базовий контролер, який створено для задання стандартної культури для вебсайту
+    /// </summary>
+    public class BaseController : Controller
+    {
+        //TODO: задати для авторизованого користувача ту культуру, яку вибрав він
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+            var culture = CultureInfo.GetCultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+    }
+}
