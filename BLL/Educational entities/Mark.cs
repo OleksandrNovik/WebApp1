1390e7b9438cf26c43884db8d25b7f3144110091
Novik Oleksandr <novikoleksandr9@gmail.com>
2023-05-06 00:39:18 +0300

    Added first versions of student and mark entities

--- BLL/Educational entities/Mark.cs
+++ BLL/Educational entities/Mark.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BLL.Educational_entities
+﻿namespace BLL.Educational_entities
 {
     public class Mark
     {
+        public int Id { get; set; }
+        public int Value { get; set; }
     }
 }
