2402c4d0fc151ccf20e3346b89a3f6e62f4cc705
Novik Oleksandr <novikoleksandr9@gmail.com>
2023-05-23 17:03:29 +0300

    Created HelperHTML class, which will help generate html code for a certain purposes

---
+++ BLL/Injections/HelperHTML.cs
@@ -0,0 +1,14 @@
+﻿namespace BLL.Injections
+{
+	public static class HelperHTML
+	{
+		public static string GenerateUnorderedList(string value)
+		{
+			var rawLinesList = value
+					.Split(new string[] { "\n\t", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+			var listItems = string.Join("", rawLinesList
+										.Select(line => line.Contains('-') ? $"<li class=\"description-list-item\">{line.Replace('-', ' ')}</li>" : $"<div>{line}</div>"));
+			return $"<ul style=\"padding: 0;\">{listItems}</ul>";
+		}
+	}
+}
